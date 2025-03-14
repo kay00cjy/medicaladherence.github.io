197821a22cde982dec88494daa132e8a334e77c0
kaylie <kayliec.jy@gmail.com>
2025-03-14 09:45:35 -0700

    Update send_data.py

--- send_data.py
+++ send_data.py
@@ -1,22 +1,24 @@
-from flask import Flask, request, jsonify
-import serial
+from flask import Flask, request, jsonify # use FlASK to create a web server
+import serial # to import pyserial for USB communication
 import time
 import json
 
-app = Flask(__name__)  # TODO fix!!
+app = Flask(__name__)  # Create a Flask web server
 
 arduino = serial.Serial('COM3', 9600, timeout=1)  
-time.sleep(2)  # Allow time for connection
+time.sleep(2)  # give time for connection
 
 @app.route('/sendData', methods=['POST'])
+
+# create a function that receives data from the web server
 def receive_data():
-    schedule_data = request.get_json()
+    schedule_data = request.get_json() # gets data from the web server
     print("Received Schedule:", schedule_data)
 
     for entry in schedule_data:
-        formatted_entry = f"{entry['compartment']},{entry['date']},{entry['time']},{entry['medication']}\n"
-        arduino.write(formatted_entry.encode())
-        time.sleep(0.5)  # Avoid buffer overflow
+        formatted_entry = f"{entry['compartment']},{entry['date']},{entry['time']},{entry['medication']}, {entry['effects']}\n"
+        arduino.write(formatted_entry.encode()) # send to ardunio
+        time.sleep(0.5)  # give time to send so buffer doesn't blow up
 
     return jsonify({"status": "Data sent to Arduino"})
 
